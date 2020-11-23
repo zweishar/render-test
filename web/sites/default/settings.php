614f0ad2f82ba92b85ffe9a1bf9fe9a333d55fc1
zweishar <zweishar@gmail.com>
2020-11-23 12:38:02 -0500

    Add local settings file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -792,9 +792,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
 $settings['config_sync_directory'] = '../config/sync';
 
 // $databases['default']['default'] = [
